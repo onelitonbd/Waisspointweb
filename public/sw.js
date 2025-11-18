6e71d31cbf22e3828fc22f5691d274e3c0865183
onelitonbd <onelitonbd@gmail.com>
2025-11-18 13:11:17 +0000

    pwa done

---
+++ public/sw.js
@@ -0,0 +1,57 @@
+// Service Worker for PWA
+const CACHE_NAME = 'study-agent-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/auth.js',
+  '/js/chat.js',
+  '/js/notes.js',
+  '/js/exams.js',
+  '/js/sidebar.js',
+  '/js/firebase-config.js',
+  '/js/markdown-renderer.js',
+  '/js/math-formatter.js',
+  '/js/gemini2-notes.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
